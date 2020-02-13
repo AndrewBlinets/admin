02b96666dc60f4e0598265343b1a709e2a4e1695
Blinec_A <blinec_a@ipps.by>
2020-02-13 08:48:05 +0300

    @CrossOrigin

--- src/main/java/by/ipps/admin/controller/DepartmentController.java
+++ src/main/java/by/ipps/admin/controller/DepartmentController.java
@@ -4,11 +4,13 @@ import by.ipps.admin.controller.base.BaseEntityAbstractController;
 import by.ipps.admin.controller.base.BaseEntityController;
 import by.ipps.admin.entity.Department;
 import by.ipps.admin.utils.resttemplate.DepartmentRestTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/department")
+@CrossOrigin
 public class DepartmentController
     extends BaseEntityAbstractController<Department, DepartmentRestTemplate>
     implements BaseEntityController<Department> {
